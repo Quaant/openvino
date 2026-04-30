c7611fa3fc8ce0a427f5c2087599abd59018e553
Quaant <egor.zhulin333@gmail.com>
2026-04-30 19:57:37 +0300

    fix tests

--- src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
+++ src/plugins/intel_cpu/tests/functional/custom/single_layer_tests/classes/activation.cpp
@@ -248,6 +248,7 @@ std::string ActivationLayerCPUTest::getPrimitiveType(const utils::ActivationType
             (activation_type == utils::ActivationTypes::Sigmoid) ||
             (activation_type == utils::ActivationTypes::SoftSign) ||
             (activation_type == utils::ActivationTypes::Sqrt) ||
+            (activation_type == utils::ActivationTypes::Swish) ||
             (activation_type == utils::ActivationTypes::Tanh))
             return "jit";
     }
